5501bf01f37ccdf4e2ef91a523a2a864adac1605
Tobias Glauser <94681693+Tobias-Glauser@users.noreply.github.com>
2022-08-23 09:46:13 +0200

    Ajout de la conversion de température

--- js/main.js
+++ js/main.js
@@ -5,11 +5,23 @@
  * @since   2019-08-19
  */
 
+function convertirDegresCToF(temperature){
+  'use strict';
+  return temperature * 9 / 5 + 32;
+}
+
 (function main() {
   'use strict'; // Demande un interprétation stricte du code
 
-  let a = 'Bonjour';
+  const temperature = Number( prompt('Température en celcius :') );
 
+  if (isNaN(temperature)) {
+    alert('Veuillez saisir une température valide');
+    return;
+  }
+  alert(`${temperature}°C = ${convertirDegresCToF(temperature)}°F`);
+
+  const a = 'bonjour';
   console.log(a);
 }()); // Main IIFE
 
